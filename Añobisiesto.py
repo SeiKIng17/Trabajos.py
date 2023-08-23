fdd5ead9b4630f2137b028b57a35fe693e161c4f
Fabian Garcia <josef.garciaa@konradlorenz.edu.co>
2023-08-22 20:11:57 -0500

    Añobisiesto

---
+++ Añobisiesto.py
@@ -0,0 +1,6 @@
+#Algoritmo para determinar si un año es bisiesto
+año = int(input("Ingrese un año: "))
+if (año % 4 == 0 and año % 100 != 0) or año % 400 == 0:
+    print("El año",año,"es bisiesto")
+else:
+    print("El año",año,"no es bisiesto")
