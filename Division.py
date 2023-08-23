f24a366b71925bdd6428f7f581e0b1f431694bfd
Fabian Garcia <josef.garciaa@konradlorenz.edu.co>
2023-08-22 20:25:05 -0500

    Division y residuo

---
+++ Division.py
@@ -0,0 +1,9 @@
+#Algoritmo para una division en python
+print("Ingrese su primer numero")
+n1 = float(input())
+print("Ingrese su segundo numero")
+n2 = float(input())
+div = n1 // n2
+print("La divison es",div)
+residuo = n1 % n2
+print("El residuo es",residuo)
